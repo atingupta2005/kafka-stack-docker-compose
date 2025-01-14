09ccde7214ba2eca93c31acf9824baed44a6dc08
Atin Gupta <atingupta@email.com>
2025-01-14 11:30:30 +0530

    -

---
+++ commands.sh
@@ -0,0 +1,33 @@
+curl -fsSL https://get.docker.com -o install-docker.sh
+sudo sh install-docker.sh
+sudo usermod -aG docker $USER
+exit
+
+mkdir -p ~/.docker/cli-plugins/
+
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+
+chmod +x ~/.docker/cli-plugins/docker-compose
+
+docker compose version
+
+
+git clone https://github.com/atingupta2005/kafka-stack-docker-compose
+
+# Get public ip address of the cloud vm and update below
+
+cd kafka-stack-docker-compose
+
+cat .env
+
+echo "EXTERNAL_PUBLIC_IP=52.170.103.92" >> .env
+
+cat .env
+
+docker compose -f full-stack-zk-multiple-kafka-multiple-full-stack-ag.yml up -d
+
+curl localhost:8080
+
+login:
+admin@admin.io
+admin
